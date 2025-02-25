bcbc510fcd801812eb1212a9fb69dd8ad59cab48
Raubwuerger <hannuschka@web.de>
2025-02-25 12:31:52 +0100

    Moved class FunctionTypes to correct path.

--- ParadoxTranslationHelper/FunctionObject/FunctionTypes.cs
+++ ParadoxTranslationHelper/FunctionObject/FunctionTypes.cs
@@ -17,5 +17,6 @@ namespace ParadoxTranslationHelper
         public static readonly string Resub = "RESUB";              // public static string FUNCTION_RESUB = "resub";
         public static readonly string Analyse = "ANALYSE";          // public static string FUNCTION_ANALYSIS = "analyse";
         public static readonly string CheckForDoubleKeys = "CHECK_FOR_DOUBLE_KEYS";
+        public static readonly string RemoveDoubleKeys = "REMOVE_DOUBLE_KEYS";
     }
 }
