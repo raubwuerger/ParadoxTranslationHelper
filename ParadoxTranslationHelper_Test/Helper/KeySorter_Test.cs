6e657fd534bea8b66bb3fa1f6de0ba11758d1268
Jürgen Hannuschka <hannuschka@web.de>
2026-03-15 15:14:57 +0100

    http://mint/issues/123 Revised test structure.

--- ParadoxTranslationHelper_Test/Helper/KeySorter_Test.cs
+++ ParadoxTranslationHelper_Test/Helper/KeySorter_Test.cs
@@ -14,6 +14,10 @@ namespace ParadoxTranslationHelper_Test
         List<TranslationFile> _localisationFilesSteam;
 
         const string fileNameSteam = "focus_l_english.yml";
+        const string fileNameGerman = "focus_l_german.yml";
+        readonly string baseTestPath = Path.Combine(Helper.GetTestLocation(), Helper.TEST_DATA, Helper.FUNCTIONS, Helper.VALIDATE, Helper.LOCALISATION);
+        readonly string baseTestPathOriginals = Path.Combine(Helper.GetTestLocation(), Helper.TEST_DATA, Helper.FUNCTIONS, Helper.VALIDATE, Helper.LOCALISATION,"german_orgs");
+        readonly string testPath_german_empty = "german_empty";
 
         [TestInitialize]
         public void TestInitialize()
@@ -22,11 +26,34 @@ namespace ParadoxTranslationHelper_Test
             _localisationFilesSteam = FileUtility.CreateTranslationFilesFromDirectory(steamFile);
         }
 
+        private bool ResetTest( string pathToClean, string pathToGetOriginal )
+        {
+            try
+            {
+                string[] filePaths = Directory.GetFiles(pathToClean);
+                foreach (string filePath in filePaths)
+                {
+                    File.Delete(filePath);
+                }
+
+                File.Copy(Path.Combine(pathToGetOriginal, fileNameGerman), Path.Combine(pathToClean, fileNameGerman));
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail();
+                return false;
+            }
+        }
+
         [TestMethod]
         [DataRow(DisplayName = "translated file null entries")]
         public void TranslatedFileNoEntries()
         {
-            String pathTranslated = Path.Combine(Helper.GetTestLocation(), Helper.TEST_DATA, Helper.FUNCTIONS, Helper.VALIDATE, Helper.LOCALISATION, "german_empty");
+            Assert.IsTrue(ResetTest(Path.Combine(baseTestPath, testPath_german_empty), Path.Combine(baseTestPathOriginals, testPath_german_empty)));
+
+            String pathTranslated = Path.Combine(Helper.GetTestLocation(), Helper.TEST_DATA, Helper.FUNCTIONS, Helper.VALIDATE, Helper.LOCALISATION, testPath_german_empty);
             List<TranslationFile> translated = FileUtility.CreateTranslationFilesFromDirectory(pathTranslated);
             
             KeySorter keySorter = new KeySorter();
@@ -37,6 +64,11 @@ namespace ParadoxTranslationHelper_Test
 
             Dictionary<int, LineObject> sorted = keySorter.SortedKeys;
             Assert.AreEqual(keySorter.OriginalKeys.Count,sorted.Count);
+
+            Assert.IsTrue(FileUtility.BackupFile(translated[0].FileNameWithBasePath));
+
+            translated[0].Lines = sorted;
+            Assert.IsTrue(FileUtility.Write(translated[0], translated[0].FileNameWithBasePath));
         }
 
         [TestMethod]
